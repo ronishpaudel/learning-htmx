1d8b8e423434d51002a817dbecb3a5f74364dd88
robuGdada <paudelronish@gmail.com>
2024-01-09 18:32:53 +0545

    search api add

--- server.ts
+++ server.ts
@@ -35,6 +35,46 @@ app.get("/users", (req, res) => {
   }, 1000);
 });
 
+//POST req for search
+const contacts = [
+  { name: "Swikar Sharma", email: "swikarsharma@gmail.com" },
+  { name: "Sandip Pokhrel", email: "sandip.m@example.com" },
+  { name: "Ronish Paudel", email: "paudelronish@gmail.com" },
+  { name: "Sunil Acharya", email: "sunil.a@example.com" },
+  { name: "Anita Lama", email: "anita.lama@gmail.com" },
+  { name: "Rajesh KC", email: "rajkc@example.com" },
+];
+
+app.post("/search", (req, res) => {
+  const searchTerm = req.body.search.toLowerCase();
+
+  if (!searchTerm) {
+    return res.send(`<tr></tr>`);
+  }
+
+  const searchResults = contacts.filter((contact) => {
+    const name = contact.name.toLowerCase();
+    const email = contact.email.toLowerCase();
+
+    return name.includes(searchTerm) || email.includes(searchTerm);
+  });
+
+  setTimeout(() => {
+    const searchResultHtml = searchResults
+      .map(
+        (contact) => `
+      <tr class="text-center">
+        <td><div class="my-4 p-2">${contact.name}</div></td>
+        <td><div class="my-4 p-2">${contact.email}</div></td>
+      </tr>
+    `
+      )
+      .join("");
+
+    res.send(searchResultHtml);
+  }, 1000);
+});
+
 app.listen(Port, () => {
   console.log(`Server up on port ${Port}`);
 });
