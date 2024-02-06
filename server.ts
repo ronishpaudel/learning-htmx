f7bee746b9a4a595d2400ae105eb4ff8424aeb64
robuGdada <paudelronish@gmail.com>
2024-02-06 17:25:23 +0545

    chore

--- server.ts
+++ server.ts
@@ -6,8 +6,10 @@ const Port = 3000;
 
 // Set static folder
 app.use(express.static("public"));
+
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: true }));
+
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
