816ebe818c78dcc1382492c7e18331e6a5af7255
robuGdada <paudelronish@gmail.com>
2024-07-03 20:26:33 +0545

    dummy data added

---
+++ utils/data.tsx
@@ -0,0 +1,9 @@
+//POST req for search
+export const contacts = [
+  { name: "Swikar Sharma", email: "swikarsharma@gmail.com" },
+  { name: "Sandip Pokhrel", email: "sandip.m@example.com" },
+  { name: "Ronish Paudel", email: "paudelronish@gmail.com" },
+  { name: "Sunil Acharya", email: "sunil.a@example.com" },
+  { name: "Anita Lama", email: "anita.lama@gmail.com" },
+  { name: "Rajesh KC", email: "rajkc@example.com" },
+];
